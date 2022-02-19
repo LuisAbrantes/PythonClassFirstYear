5352a323870f666207f77120513e3226d1da82f2
Luis Abrantes <luis.hsa@gmail.com>
2022-02-19 13:08:02 -0300

    Exemplos com a função "type()"

--- class02.py
+++ class02.py
@@ -1,6 +1,17 @@
-name = "Jack"
-print(name)
+str = "Welcome to Guru99"
+run = 100
+rr = 7.7
+complex_num = 19j+21
+player_list = ["VK", "RS", "JB", "RP"]
+bat_name = ("A", "B", "C", "D")
+duckworth = {"A": "a", "B": "b", "C": "c", "D": "d"}
+lbw = {'Y', 'N', 'T', 'F', 'T'}
 
-name = "Angela"
-print(name)
-
+print("The type is : ", type(str))
+print("The type is : ", type(run))
+print("The type is : ", type(rr))
+print("The type is : ", type(complex_num))
+print("The type is : ", type(player_list))
+print("The type is : ", type(bat_name))
+print("The type is : ", type(duckworth))
+print("The type is : ", type(lbw))
