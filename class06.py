0360d6e9d8a4c8f10c0ba9719df0ee7dc70f5c7e
LuisAbrantes <luis.hsa@gmail.com>
2022-02-24 18:37:49 -0300

    media aritimetica

--- class06.py
+++ class06.py
@@ -9,9 +9,23 @@
 
 
 
-idade = int(input("Digite sua idade: "))
+# idade = int(input("Digite sua idade: "))
 
-if (idade >= 18):
-    print("Pode entrar na balada!")
-else:
-    print("Aluno menor de idade. Não pode entrar na balada!")
+# if (idade >= 18):
+#     print("Pode entrar na balada!")
+# else:
+#     print("Aluno menor de idade. Não pode entrar na balada!")
+    
+
+
+
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+n3 = float(input("Digite a terceira nota: "))
+n4 = float(input("Digite a quarta nota: "))
+
+ media = (n1+n2+n3+n4)/2
+ if (media <= 6):
+     print("Ta de rec")
+ else:
+     print("Passou de ano")
