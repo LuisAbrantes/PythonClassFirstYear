87af85360f1c4480f7edd1ad7895e50b24d16d8c
LuisAbrantes <luis.hsa@gmail.com>
2022-02-24 18:19:50 -0300

    primeiro problema de logica

---
+++ class06.py
@@ -0,0 +1,17 @@
+#AULA SOBRE LÓGICA
+
+# igual: ==
+# diferente: !=
+# maior: >
+# menor: <
+# maior ou igual: >=
+# menor ou igual: <=
+
+
+
+idade = int(input("Digite sua idade: "))
+
+if (idade >= 18):
+    print("Pode entrar na balada!")
+else:
+    print("Aluno menor de idade. Não pode entrar na balada!")
