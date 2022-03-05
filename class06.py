584a07fcd03443fb208a299fd1a1ee15a94fbb78
Luis Abrantes <luis.hsa@gmail.com>
2022-03-05 20:39:41 -0300

    correção class06

--- class06.py
+++ class06.py
@@ -24,8 +24,8 @@ n2 = float(input("Digite a segunda nota: "))
 n3 = float(input("Digite a terceira nota: "))
 n4 = float(input("Digite a quarta nota: "))
 
- media = (n1+n2+n3+n4)/2
- if (media <= 6):
-     print("Ta de rec")
- else:
-     print("Passou de ano")
+media = (n1+n2+n3+n4)/2
+if (media <= 6):
+    print("Ta de rec")
+else:
+    print("Passou de ano")
