b64dd47da12f014e5feb93236853d6d314b059ab
Luis Abrantes <luis.hsa@gmail.com>
2022-03-07 00:07:45 -0300

    Treasure map corrigido

--- class010.py
+++ class010.py
@@ -14,3 +14,5 @@ position = input("Where do you want to put the treasure? ")
 horizontal = int(position[0])
 vertical = int(position[1])
 map[vertical - 1][horizontal - 1] = "X"
+
+print(f"{row1}\n{row2}\n{row3}")
