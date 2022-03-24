ff6f6c37b3126e2bcfc1c4ee4eb7a128a016c806
LuisAbrantes <luis.hsa@gmail.com>
2022-03-24 14:21:12 -0300

    class011 correção

--- class011.py
+++ class011.py
@@ -1,9 +1,9 @@
 num1 = int(input("Digite o primeiro número para comparação: "))
 num2 = int(input("Digite o segundo número para comparação: "))
 
-if num1>num2:
+if (num1>num2):
     print("O primeiro número é maior.")
-else num1>num2:
+elif num1>num2:
     print("O segundo número é maior.")
 elif num1==num2:
     print("Tente dois números diferentes!")
