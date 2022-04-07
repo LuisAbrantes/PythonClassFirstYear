7f342f41dc63801fc01af108aba7b091f981ed09
LuisAbrantes <luis.hsa@gmail.com>
2022-04-07 18:54:17 -0300

    class019

---
+++ class019.py
@@ -0,0 +1,68 @@
+#tabuada do 10
+contador=100
+while(contador >=0):
+    print(contador)
+    contador = contador - 10
+print("Tabuada do 10")
+
+#tabuada do 19
+contador=90
+while(contador >=0):
+    print(contador)
+    contador = contador - 9
+print("Tabuada do 9")
+
+#tabuada do 8
+contador=80
+while(contador >=0):
+    print(contador)
+    contador = contador - 8
+print("Tabuada do 8")
+
+#tabuada do 7
+contador=70
+while(contador >=0):
+    print(contador)
+    contador = contador - 7
+print("Tabuada do 7")
+
+#tabuada do 6
+contador=60
+while(contador >=0):
+    print(contador)
+    contador = contador - 6
+print("Tabuada do 6")
+
+#tabuada do 5
+contador=50
+while(contador >=0):
+    print(contador)
+    contador = contador - 5
+print("Tabuada do 5")
+
+#tabuada do 4
+contador=40
+while(contador >=0):
+    print(contador)
+    contador = contador - 4
+print("Tabuada do 4")
+
+#tabuada do 3
+contador=30
+while(contador >=0):
+    print(contador)
+    contador = contador - 3
+print("Tabuada do 3")
+
+#tabuada do 2
+contador=20
+while(contador >=0):
+    print(contador)
+    contador = contador - 2
+print("Tabuada do 2")
+
+contador=10
+while(contador >=0):
+    print(contador)
+    contador = contador - 1
+print("Tabuada do 1")
