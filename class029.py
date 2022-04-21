e4d1985bd70c3c09933a37057836f7c152e1e92d
LuisAbrantes <luis.hsa@gmail.com>
2022-04-21 18:35:07 -0300

    class029 Atividade / Exercicio 3

---
+++ class029.py
@@ -0,0 +1,7 @@
+#Atividade 1 / Exercicio 3
+
+base = int(input("Insira a base do retângulo: "))
+altura = int(input("Insira a altura do retângulo: "))
+
+area = base * altura
+print(area, "metros quadrados.")
