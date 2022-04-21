be1200abb10964e2960c1e5e5e9a7ce9f9c9a7ce
LuisAbrantes <luis.hsa@gmail.com>
2022-04-21 11:03:53 -0300

    class028 Atividade 1 / Exercicio 2

---
+++ class028.py
@@ -0,0 +1,7 @@
+#Atividade 1 / Exercicio 2
+
+conta = ((2*8)+(5**2)*((1-5)**2))/2
+print("O número que dividiremos por 10 é: ", conta)
+
+conta2 = conta / 10
+print("O número dividido por 10 é: ", conta2)
