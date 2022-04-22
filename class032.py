79bb2d73217bdee0603d7dc97cb5823402809b3b
Luis Abrantes <luis.hsa@gmail.com>
2022-04-22 01:15:53 -0300

    Adicionado um exemplo para converter temperatura com PyQT5.

---
+++ class032.py
@@ -0,0 +1,79 @@
+# Como instalar o PyQt5
+# pip install pyqt5
+# ou
+# pip3 install pyqt5
+
+from PyQt5 import QtCore, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(300, 140)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(10, 40, 121, 21))
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(160, 40, 121, 21))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(110, 70, 100, 30))
+        self.pushButton.setObjectName("pushButton")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(160, 20, 61, 16))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(10, 20, 61, 16))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(140, 40, 21, 20))
+        self.label_4.setObjectName("label_4")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 299, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+        self.pushButton.clicked.connect(self.Converteither)  # Mod here
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Exercício 3"))
+        self.pushButton.setText(_translate("MainWindow", "Converter"))
+        self.label_2.setText(_translate("MainWindow", "Fahrenheit"))
+        self.label_3.setText(_translate("MainWindow", "Celsius"))
+        self.label_4.setText(_translate("MainWindow", "="))
+
+    def Celsius_Line(self):
+        c = str(float(self.lineEdit.text()) * (9/5) + 32)
+        self.lineEdit_2.setText(c)
+
+    def Fahrenheit_Line(self):
+        f = str(float(self.lineEdit_2.text()) * (9/5) - 32)
+        self.lineEdit.setText(f)
+
+    def Converteither(self):
+        if self.lineEdit.text() == "":
+            f = str(float(self.lineEdit_2.text()) * (9/5) - 32)
+            self.lineEdit.setText(f)
+        else:
+            c = str(float(self.lineEdit.text()) * (9/5) + 32)
+            self.lineEdit_2.setText(c)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
