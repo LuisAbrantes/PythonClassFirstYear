72addd362c992cd658d79071c8fcde2a8ade23de
LuisAbrantes <luis.hsa@gmail.com>
2022-04-22 10:10:16 -0300

    class033 Atividade 2 / Exercício 4

---
+++ class033.py
@@ -0,0 +1,11 @@
+# Atividade 2 / Exercício 4
+
+num1 = int(input("Digite o primeiro número para comparação: "))
+num2 = int(input("Digite o segundo número para comparação: "))
+
+if (num1>num2):
+    print("O primeiro número é maior.")
+elif(num1<num2):
+    print("O segundo número é maior.")
+else:
+    print("Tente dois números diferentes!")
