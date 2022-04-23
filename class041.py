56e809b32c2fdfbe7b9b503230f28f1b43b1aa20
LuisAbrantes <luis.hsa@gmail.com>
2022-04-23 16:52:17 -0300

    class041 Atividade 4 / Exercicio 1

---
+++ class041.py
@@ -0,0 +1,12 @@
+# Atividade 4 / Exercicio 1
+
+n1 = float(input("Digite um número diferente de zero para ser analisado: "))
+if n1 > 0:
+    n2 = n1**2
+    print(n2)
+elif: n1 < 0:
+    n2 = n1**(1/2)
+    print(n2)
+else:
+    print("Digite um número diferente de 0.")
+ 
