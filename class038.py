004df7f46fddd0bacd472c5a9dc2ea1f0afcf250
LuisAbrantes <luis.hsa@gmail.com>
2022-04-23 14:01:36 -0300

    class038 Atividade 2 / Exercicio 8

---
+++ class038.py
@@ -0,0 +1,8 @@
+# Atividade 2 / Exercicio 8
+
+b_ma = int(input("Insira a base maior do trapézio: "))
+b_me = int(input("Insira a base menor do trapézio: "))
+alt = int(input("Insira a altura do trapézio: "))
+
+a_trap = (b_ma+b_me)*alt
+print("A área do trapézio é ",a_trap, "metros quadrados")
