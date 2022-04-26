6933a2d31b0911b3455a8ac20fc68a8281abc2c7
Luis Abrantes <luis.hsa@gmail.com>
2022-04-26 00:01:39 -0300

    class045 Atividade 4 / Exercicio 5

---
+++ class045.py
@@ -0,0 +1,58 @@
+#Atividade 4 / Exercicio 5
+
+contador = 0
+#pergunta 1
+print("A música mais ouvida do spotify foi 'Cavalo de Tróia'. Digite - S para sim e - N para não.")
+acertou = False
+while not acertou:
+    jogador = str(input(""))
+    if jogador == "N":
+        print("Acertou!")
+        contador+=1
+    else:
+        print("Errou!")
+    break
+
+print()
+
+#pergunta 2
+print("Neymar tem uma Champions League. Digite - S para sim e - N para não.")
+acertou = False
+while not acertou:
+    jogador = str(input(""))
+    if jogador == "S":
+        print("Acertou!")
+        contador += 1
+    else:
+        print("Errou!")
+    break
+
+print()
+
+# pergunta 3
+print("Zlatan Ibrahimovic tem mais de 50 gols em Champions League. Digite - S para sim e - N para não.")
+acertou = False
+while not acertou:
+    jogador = str(input(""))
+    if jogador == "S":
+        print("Acertou!")
+        contador += 1
+    else:
+        print("Errou! Ibrahimovic tem 49 gols.")
+    break
+
+print()
+
+#pergunta 4
+print("Mc Kevin que canta a música 'O Menino Encantou a Quebrada'. Digite - S para sim e - N para não.")
+acertou = False
+while not acertou:
+    jogador = str(input(""))
+    if jogador == "S":
+        print("Acertou!")
+        contador += 1
+    else:
+        print("Errou!")
+    break
+
+print(f"Você acertou {contador}x.")
