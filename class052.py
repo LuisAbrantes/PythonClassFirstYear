fb35aaade0c66caf67f8814e13a9363c93f10277
Luis Abrantes <luis.hsa@gmail.com>
2022-04-27 09:42:35 -0300

    class052 Fibonacci

---
+++ class052.py
@@ -0,0 +1,22 @@
+# Fibonacci
+
+print("-"*30)
+print("Sequência de Fibonacci")
+print("-"*30)
+
+n = int(input("Quantos tempos você quer saber mostrar? "))
+t1 = 0
+t2 = 1
+
+print("~"*100)
+print(f"{t1} --> {t2} ", end="")
+cont = 3
+while cont <= n:
+    t3 = t1 + t2
+    print(f"--> {t3} ", end="")
+    t1 = t2
+    t2 = t3
+    cont += 1
+print(f"\nO {n}o termo é {t3}.")
+print("\nFIM")
+print("~"*100)
