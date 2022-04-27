c22626ed60180af87a3a303c50ec4cd2411df1aa
Luis Abrantes <luis.hsa@gmail.com>
2022-04-27 10:09:34 -0300

    class053 Fibonacci corrigido2

--- class052.py
+++ class052.py
@@ -10,13 +10,13 @@ t2 = 1
 
 print("~"*100)
 print(f"{t1} --> {t2} ", end="")
-cont = 3
-while cont <= n:
+contador = 3
+while contador <= n:
     t3 = t1 + t2
     print(f"--> {t3} ", end="")
     t1 = t2
     t2 = t3
-    cont += 1
+    contador += 1
 print(f"\nO {n}o termo é {t3}.")
 print("\nFIM")
 print("~"*100)
