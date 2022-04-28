05c08b8cf151a2b4b6e9e889fa9433896ed41554
Luis Abrantes <luis.hsa@gmail.com>
2022-04-28 09:46:14 -0300

    class054 tabuadas

---
+++ class054.py
@@ -0,0 +1,41 @@
+# tabuadas
+
+contador2 = 1
+while(contador2<=10):
+    print(f"Tabuada de soma do {contador2}")
+    contador = 1
+    while (contador <= 10):
+        var = contador2 + contador 
+        print(f"{contador2} + {contador} = {var}")
+        contador += 1
+    contador2 += 1
+
+contador2 = 1
+while(contador2<=10):
+    print(f"Tabuada de subtração do {contador2}")
+    contador = 1
+    while (contador <= 10):
+        var = contador2 - contador
+        print(f"{contador2} - {contador} = {var}")
+        contador += 1
+    contador2 += 1
+
+contador2 = 1
+while(contador2<=10):
+    print(f"Tabuada de multiplicação do {contador2}")
+    contador = 1
+    while (contador <= 10):
+        var = contador2 * contador
+        print(f"{contador2} * {contador} = {var}")
+        contador += 1
+    contador2 += 1
+
+contador2 = 1
+while(contador2<=10):
+    print(f"Tabuada de divisão do {contador2}")
+    contador = 1
+    while (contador <= 10):
+        var = contador2 / contador
+        print(f"{contador2} / {contador} = {var}")
+        contador += 1
+    contador2 += 1
