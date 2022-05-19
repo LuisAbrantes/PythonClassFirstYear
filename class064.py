cd4d55a33a7c98b98d772e084c1e706bebeecee5
LuisAbrantes <luis.hsa@gmail.com>
2022-05-19 17:57:36 -0300

    class064

--- class064.py
+++ class064.py
@@ -1,17 +1,25 @@
 '''
 Faça um programa para guardar os nomes de diversos alunos de uma escola, e apresente ao final a lista de todos os nomes dos alunos cadastrados.
 '''
-alunos = list()
+medias = list()
+nomes = list()
 
 qtdeAluno = int(input("Qual é a quantidade de alunos? "))
 
 contador = 0 
-while contador < qtdeAluno:
-    aluno = input("Digite o nome de um aluno: ")
-    alunos.append(aluno)
+while(contador < qtdeAluno):
+    n = input("Digite o nome de um aluno: ")
+    v = float(input("Nota: "))
+
+    nomes.append(n)
+    medias.append(v)
+
     contador += 1
 
+contador = 0
+while(contador<qtdeAluno):
+    if medias[contador] >= 6.0:
+        print(nomes[contador])
 
-
-print(alunos)
+    contador += 1  
 
