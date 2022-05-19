988311422b995293d2ea2dcbe886eef5310a38dc
LuisAbrantes <luis.hsa@gmail.com>
2022-05-19 14:05:37 -0300

    class062 listas

---
+++ class062.py
@@ -0,0 +1,17 @@
+#Introdução à LISTAS
+
+notas = list()
+
+#append serve para adidionar um novo valor à lista ao FINAL dela.
+notas.append(8.5)
+notas.append(6.0)
+notas.append(10)
+notas.append(5)
+
+#calcular a média:
+#Lista sempre aparece entre []
+
+soma = notas[0] + notas[1] + notas[2] + notas[3]
+media = soma/4
+
+print(notas)
