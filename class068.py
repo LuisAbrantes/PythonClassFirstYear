8173e85ec29119667259e1c1191f76dcc11d3bbd
LuisAbrantes <luis.hsa@gmail.com>
2022-05-26 14:16:16 -0300

    class068

---
+++ class068.py
@@ -0,0 +1,8 @@
+# Os valores de imposto de importações para os estados de MG, SP, TO, GO, RJ:
+# 5, 7, 3, 10 e 1, respectivamente. Apresente a soma dos impostos de São Paulo e Goiás.
+
+#SP - 5; GO - 10.
+
+lista = [5, 7, 3, 10, 1]
+soma = lista[1]+lista[3]
+print(f"A soma dos impostos de São Paulo e Goiás é: {soma}")
