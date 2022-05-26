63dc2453a3bf500996c22aa3503ac807ab1c2874
LuisAbrantes <luis.hsa@gmail.com>
2022-05-26 14:10:17 -0300

    class067

---
+++ class067.py
@@ -0,0 +1,12 @@
+#criar um programa para somar o segundo com o terceiro número
+#somar o quadrado do quinto número com o quadrado do terceiro número
+
+lista = [1, 2, 3, 4, 5]
+
+soma = lista[1]+lista[2]
+print(f"A soma do segundo com o terceiro número é: {soma}.")
+
+quadradoTres = lista[2]**2
+quadradoCinco = lista[4]**2
+somaQuadrado = quadradoCinco+quadradoTres
+print(f"A soma dos quadrados do terceiro e do quinto número é: {somaQuadrado}.")
