f3dfb6815ebc2cf60ad0c54f02dc5e2ff51cc05c
Luis Abrantes <luis.hsa@gmail.com>
2022-05-29 14:53:07 -0300

    class074

---
+++ class074.py
@@ -0,0 +1,14 @@
+contador = 1
+acumulador = 1
+
+while contador <= 10:
+    print(contador, acumulador)
+
+    if contador > 5:
+        break
+
+    acumulador += contador
+    contador += 1
+else: 
+    print("Cheguei no else!")
+print("Cheguei no else!")
