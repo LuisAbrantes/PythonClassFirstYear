c2131a66741872d3ac4493f5f16e1a3564222dbe
Luis Abrantes <luis.hsa@gmail.com>
2022-05-29 15:30:46 -0300

    class076

---
+++ class076.py
@@ -0,0 +1,13 @@
+texto = "Python"
+nova_string = ""
+
+for letra in texto:
+    if letra == "t":
+        nova_string += letra.upper()
+    elif letra == "h":
+        nova_string += letra.upper()
+        break
+    else:
+        nova_string += letra
+
+print(nova_string)
