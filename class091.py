562ff09e901e5d8bb12f328c4bf11510885e9bd5
Luis Abrantes <luis.hsa@gmail.com>
2022-08-03 22:42:27 -0300

    Tuplas

---
+++ class091.py
@@ -0,0 +1,17 @@
+#Vídeo 10 - Estudo de Tuplas.
+
+from tokenize import Double
+
+
+t1 = (1, 2, 3, 4, 5)
+t1 = list(t1)
+t1[1] = 3000
+t1 = tuple(t1)
+
+t2 = (10, 20, "Andre")
+a, c, _ = t2
+
+print(t1)
+
+def CalculaResultante(vetorEmX : float, vetorEmY:float) -> tuple(float, float):
+    return 0.0
