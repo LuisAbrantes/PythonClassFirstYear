62bf2f3056522b54f12f6dbd0a743d4a43c9ea21
Luis Abrantes <luis.hsa@gmail.com>
2022-08-06 04:21:40 -0300

    Sexta parte sobre "dicionário" do curso.

---
+++ class104.py
@@ -0,0 +1,14 @@
+lista = [
+    ("c1", 1),
+    ("c2", 2), 
+    ("c3", 3),
+    ["c4", 4],
+    ["c5", 5],
+    ["c6", 6],
+]
+
+d1 = dict(lista)
+print(d1)
+"""A lista com listas e tuplas virou um dicionário. 
+A Lista "lista" pode ser uma Tupla também. Ou seja, pode ser Lista ou Tupla em qualquer lugar
+que não tem diferença ao converter a Dicionário"""
