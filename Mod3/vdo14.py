9398c8029e4468b9784751f7d757dc7a3913c942
Luis Abrantes <luis.hsa@gmail.com>
2022-08-09 01:29:15 -0300

    Proposta do exercício - módulo 3; vídeo 14.

---
+++ Mod3/vdo14.py
@@ -0,0 +1,16 @@
+"""
+--> É uma lista de números internos;
+--> As listas internas têm o tamanho de 10 elementos;
+--> As listas internas contém números de 1 a 10 e eles podem ser duplicados.
+
+Exercício 
+
+--> Crie uma função que encontra os dois primeiros números duplicados na lista interna.
+        Requisitos:
+            A ordem dos números duplicados é considerada a partir da segunda ocorrência
+            (o número duplicado em si).
+            Exemplo:
+                [1, 2, 3, ->3<-, 2, 1] -> 1, 2, 3 estão duplicados, (retorne 3)
+                [1, 2, 3, 4, 5, 6] -> Retorne -1(não tem duplicados)
+            Se não encontrar duplicados na lista, retorne -1.
+"""
