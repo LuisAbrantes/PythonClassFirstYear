31b221c96f2b0488f97b44a4e533b4a855f08ca6
LuisAbrantes <luis.hsa@gmail.com>
2022-08-09 18:43:56 -0300

    Protótipo do exercício dado em sala "lista de nomes". Possivelmente correto.

---
+++ class111.py
@@ -0,0 +1,16 @@
+'''
+Faça um programa que crie uma função para 
+obter o nome de pessoas de maneira indefinida, 
+e armazene esses nomes em uma lista.
+Em seguida, apresente os nomes da lista.
+'''
+
+def criaLista():
+
+    resp = 1
+    listaNomes = list()
+    while(resp != 0):
+        nome = input("Digite o nome: ")
+        listaNomes.append(nome)
+        resp = int(input("Deseja dicionar mais um nome? (0 para sair) "))
+    print(listaNomes)
